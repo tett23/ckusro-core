69d765a53e43d92ccc196615d10cab913859d366
tett23 <tett23@gmail.com>
2019-02-10 14:59:48 +0900

    Split test functions

--- src/namespace.rs
+++ src/namespace.rs
@@ -112,120 +112,136 @@ impl ToString for NamespaceType {
 mod tests {
   use super::*;
 
-  #[test]
-  fn test_namespace_new_domain() {
-    let actual = Namespace::new(NamespaceType::Domain, "github.com");
-    let expected = Namespace {
-      name: "github.com".to_owned(),
-      namespace_type: NamespaceType::Domain,
-    };
+  mod namespace {
+    use super::*;
 
-    assert_eq!(actual, expected);
+    #[test]
+    fn test_new_domain() {
+      let actual = Namespace::new(NamespaceType::Domain, "github.com");
+      let expected = Namespace {
+        name: "github.com".to_owned(),
+        namespace_type: NamespaceType::Domain,
+      };
+
+      assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_new_user() {
+      let actual = Namespace::new(NamespaceType::User, "tett23");
+      let expected = Namespace {
+        name: "tett23".to_owned(),
+        namespace_type: NamespaceType::User,
+      };
+
+      assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_new_repository() {
+      let actual = Namespace::new(NamespaceType::Repository, "ckusro-core");
+      let expected = Namespace {
+        name: "ckusro-core".to_owned(),
+        namespace_type: NamespaceType::Repository,
+      };
+
+      assert_eq!(actual, expected);
+    }
   }
 
-  #[test]
-  fn test_namespace_new_user() {
-    let actual = Namespace::new(NamespaceType::User, "tett23");
-    let expected = Namespace {
-      name: "tett23".to_owned(),
-      namespace_type: NamespaceType::User,
-    };
+  mod domain_namespace {
+    use super::*;
 
-    assert_eq!(actual, expected);
+    #[test]
+    fn test_new() {
+      let namespace = Namespace::new(NamespaceType::Domain, "github.com");
+      let actual = DomainNamespace::new(&namespace);
+
+      assert!(actual.is_ok());
+    }
+
+    #[test]
+    fn test_new_when_passed_invalid_namespace_type() {
+      let namespace = Namespace::new(NamespaceType::User, "tett23");
+      let actual = DomainNamespace::new(&namespace);
+
+      assert!(actual.is_err());
+    }
+
+    #[test]
+    fn test_raw() {
+      let namespace = Namespace::new(NamespaceType::Domain, "github.com");
+      let domain = DomainNamespace {
+        namespace: &namespace,
+      };
+      let actual = domain.raw();
+      let expected = &namespace;
+
+      assert_eq!(actual, expected);
+    }
   }
 
-  #[test]
-  fn test_namespace_new_repository() {
-    let actual = Namespace::new(NamespaceType::Repository, "ckusro-core");
-    let expected = Namespace {
-      name: "ckusro-core".to_owned(),
-      namespace_type: NamespaceType::Repository,
-    };
+  mod user_namespace {
+    use super::*;
 
-    assert_eq!(actual, expected);
+    #[test]
+    fn test_new() {
+      let namespace = Namespace::new(NamespaceType::User, "tett23");
+      let actual = UserNamespace::new(&namespace);
+
+      assert!(actual.is_ok());
+    }
+
+    #[test]
+    fn test_new_when_passed_invalid_namespace_type() {
+      let namespace = Namespace::new(NamespaceType::Domain, "github.com");
+      let actual = UserNamespace::new(&namespace);
+
+      assert!(actual.is_err());
+    }
+
+    #[test]
+    fn test_raw() {
+      let namespace = Namespace::new(NamespaceType::User, "tett23");
+      let user = UserNamespace {
+        namespace: &namespace,
+      };
+      let actual = user.raw();
+      let expected = &namespace;
+
+      assert_eq!(actual, expected);
+    }
   }
 
-  #[test]
-  fn test_domain_namespace_new() {
-    let namespace = Namespace::new(NamespaceType::Domain, "github.com");
-    let actual = DomainNamespace::new(&namespace);
+  mod repository_namespace {
+    use super::*;
 
-    assert!(actual.is_ok());
-  }
+    #[test]
+    fn test_new() {
+      let namespace = Namespace::new(NamespaceType::Repository, "ckusro-core");
+      let actual = RepositoryNamespace::new(&namespace);
 
-  #[test]
-  fn test_domain_namespace_new_when_passed_invalid_namespace_type() {
-    let namespace = Namespace::new(NamespaceType::User, "tett23");
-    let actual = DomainNamespace::new(&namespace);
+      assert!(actual.is_ok());
+    }
 
-    assert!(actual.is_err());
-  }
+    #[test]
+    fn test_new_when_passed_invalid_namespace_type() {
+      let namespace = Namespace::new(NamespaceType::User, "tett23");
+      let actual = RepositoryNamespace::new(&namespace);
 
-  #[test]
-  fn test_domain_namespace_raw() {
-    let namespace = Namespace::new(NamespaceType::Domain, "github.com");
-    let domain = DomainNamespace {
-      namespace: &namespace,
-    };
-    let actual = domain.raw();
-    let expected = &namespace;
+      assert!(actual.is_err());
+    }
 
-    assert_eq!(actual, expected);
-  }
+    #[test]
+    fn test_raw() {
+      let namespace = Namespace::new(NamespaceType::Repository, "ckusro-core");
+      let repository = RepositoryNamespace {
+        namespace: &namespace,
+      };
+      let actual = repository.raw();
+      let expected = &namespace;
 
-  #[test]
-  fn test_user_namespace_new() {
-    let namespace = Namespace::new(NamespaceType::User, "tett23");
-    let actual = UserNamespace::new(&namespace);
-
-    assert!(actual.is_ok());
-  }
-
-  #[test]
-  fn test_user_namespace_new_when_passed_invalid_namespace_type() {
-    let namespace = Namespace::new(NamespaceType::Domain, "github.com");
-    let actual = UserNamespace::new(&namespace);
-
-    assert!(actual.is_err());
-  }
-
-  #[test]
-  fn test_repository_namespace_new() {
-    let namespace = Namespace::new(NamespaceType::Repository, "ckusro-core");
-    let actual = RepositoryNamespace::new(&namespace);
-
-    assert!(actual.is_ok());
-  }
-
-  #[test]
-  fn test_repository_namespace_new_when_passed_invalid_namespace_type() {
-    let namespace = Namespace::new(NamespaceType::User, "tett23");
-    let actual = RepositoryNamespace::new(&namespace);
-
-    assert!(actual.is_err());
-  }
-
-  #[test]
-  fn test_user_namespace_raw() {
-    let namespace = Namespace::new(NamespaceType::User, "tett23");
-    let user = UserNamespace {
-      namespace: &namespace,
-    };
-    let actual = user.raw();
-    let expected = &namespace;
-
-    assert_eq!(actual, expected);
-  }
-
-  #[test]
-  fn test_repository_namespace_raw() {
-    let namespace = Namespace::new(NamespaceType::Repository, "ckusro-core");
-    let repository = RepositoryNamespace {
-      namespace: &namespace,
-    };
-    let actual = repository.raw();
-    let expected = &namespace;
-
-    assert_eq!(actual, expected);
+      assert_eq!(actual, expected);
+    }
   }
 }
