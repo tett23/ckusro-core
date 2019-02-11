6cec668cc76492f4d36be536875e7e6be02347e3
tett23 <tett23@gmail.com>
2019-02-11 14:18:12 +0900

    Implement PathFragment::parse_fragment

--- src/path_fragment.rs
+++ src/path_fragment.rs
@@ -1,14 +1,36 @@
 use super::error::Error;
 
+#[derive(PartialEq, Debug)]
 pub struct PathFragment {
-  domain: String,
-  user: String,
-  repository: String,
+  pub domain: String,
+  pub user: String,
+  pub repository: String,
 }
 
 impl PathFragment {
   const USER_SEPARATOR: char = '@';
   const REPOSITORY_SEPARATOR: char = ':';
+
+  pub fn parse_fragment(fragment: &str) -> Result<PathFragment, Error> {
+    let (domain, user, repository) = PathFragment::split_fragment_string(fragment)?;
+
+    Ok(PathFragment {
+      domain: domain.to_owned(),
+      user: user.to_owned(),
+      repository: repository.to_owned(),
+    })
+  }
+
+  fn split_fragment_string(fragment: &str) -> Result<(&str, &str, &str), Error> {
+    if !PathFragment::is_valid_format(fragment) {
+      return Err(Error::MalformedFragment(fragment.to_owned()));
+    }
+
+    let (domain, rest) = PathFragment::split_domain_and_rest(fragment)?;
+    let (user, repository) = PathFragment::split_user_and_repository(rest)?;
+
+    Ok((domain, user, repository))
+  }
 
   fn is_valid_format(fragment: &str) -> bool {
     let user_separator_pos = fragment.find('@');
@@ -79,6 +101,66 @@ mod tests {
 
   mod path_fragment {
     use super::*;
+
+    mod parse_fragment {
+      use super::*;
+
+      #[test]
+      fn it_works() {
+        let fragment = "github.com@tett23:ckusro-core";
+        let actual = PathFragment::parse_fragment(fragment);
+        let expected = Ok(PathFragment {
+          domain: "github.com".to_owned(),
+          user: "tett23".to_owned(),
+          repository: "ckusro-core".to_owned(),
+        });
+
+        assert_eq!(actual, expected);
+      }
+
+      #[test]
+      fn it_does_not_works() {
+        let data = vec![
+          "",
+          "tett23:ckusro-core",
+          "github.com@tett23",
+          "ckusro-core:github.com@tett23",
+        ];
+        for datum in data {
+          let actual = PathFragment::parse_fragment(datum);
+
+          assert!(actual.is_err());
+        }
+      }
+    }
+
+    mod split_fragment_string {
+      use super::*;
+
+      #[test]
+      fn it_works() {
+        let fragment = "github.com@tett23:ckusro-core";
+        let actual = PathFragment::split_fragment_string(fragment);
+        let expected = Ok(("github.com", "tett23", "ckusro-core"));
+
+        assert_eq!(actual, expected);
+      }
+
+      #[test]
+      fn it_does_not_works() {
+        let data = vec![
+          "",
+          "tett23:ckusro-core",
+          "github.com@tett23",
+          "ckusro-core:github.com@tett23",
+        ];
+        for datum in data {
+          let actual = PathFragment::split_fragment_string(datum);
+
+          assert!(actual.is_err());
+        }
+      }
+    }
 
     mod is_valid_format {
       use super::*;
